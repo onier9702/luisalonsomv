66ac2cb259517faed99c0f49d1c8c682422246bd
onier9702 <onier0217@gmail.com>
2023-04-09 17:53:27 -0600

    A simple fix-removing unnecessary imports

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Request, Response } from 'express';
-import path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
